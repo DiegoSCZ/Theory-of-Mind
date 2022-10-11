24aa1e71d4bb2886dc3c4fda15e9f71cc80c0da4
diegoSCZ <diegostchz@gmail.com>
2022-10-11 10:00:12 -0300

    se agrega sujeto

--- Theoryofmind.py
+++ Theoryofmind.py
@@ -1,5 +1,9 @@
 import tomsup as ts
+import random
+import numpy as np
+
 #iniciar el juego de emparejar monedas#
-penny = ts.PayoffMatrix(name='penny_competitive') #se agrega matriz#
+penny = ts.PayoffMatrix(name='penny_competitive')  #se agrega matriz#
 print(penny)
 
+jung = ts.RB(bias=0.7) #creación del agente que competirá que escogerá 1 el 70% de las veces#
