a8612440111715fca5826c757bb08e1306b0e868
DiegoSCZ <68380801+DiegoSCZ@users.noreply.github.com>
2022-10-11 00:57:38 -0300

    Update Theoryofmind.py

--- Theoryofmind.py
+++ Theoryofmind.py
@@ -1,5 +1,5 @@
 import tomsup as ts
 #iniciar el juego de emparejar monedas#
-penny = ts.PayoffMatrix(name='penny_competitive')
+penny = ts.PayoffMatrix(name='penny_competitive') #se agrega matriz#
 print(penny)
 
