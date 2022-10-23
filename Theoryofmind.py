79c2078dcb682bf99e2740fbd1fe66819cb93d5f
diegoSCZ <diegostchz@gmail.com>
2022-10-23 01:09:58 -0300

    se agrega prueba de sujeto jung

--- Theoryofmind.py
+++ Theoryofmind.py
@@ -2,8 +2,22 @@ import tomsup as ts
 import random
 import numpy as np
 
-#iniciar el juego de emparejar monedas#
-penny = ts.PayoffMatrix(name='penny_competitive')  #se agrega matriz#
+# iniciar el juego de emparejar monedas#
+penny = ts.PayoffMatrix(name='penny_competitive')  # se agrega matriz#
 print(penny)
 
-jung = ts.RB(bias=0.7) #creación del agente que competirá que escogerá 1 el 70% de las veces#
+jung = ts.RB(bias=0.7)  # creación del agente (RB: random bias) que competirá que escogerá 1 el 70% de las veces#
+
+# examine agent
+print(f"jung is a class of type: {type(jung)}")
+if isinstance(jung, ts.Agent):
+    print(f"but jung is also an instance of the parent class ts.Agent")
+
+#hacer que jung elija una opción
+
+choice = jung.compete()
+
+print(f'jung chose {choice} and his probabiliy fo choosing 1 was {jung.get_bias()}.')
+
+
+
